cdb7b74ded6f3c612f97879f84a8c7df96299771
JIgorSilva <jigorsilva@hotmail.com>
2024-12-18 12:32:43 -0300

    fix(Correção do retorno do metodo Post)

--- src/main/java/com/example/apirest/controller/UserController.java
+++ src/main/java/com/example/apirest/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.example.apirest.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.apirest.model.UserApi;
 import com.example.apirest.repository.UserRepository;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -16,14 +21,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/api/users")
 public class UserController {
     private UserRepository repositoryUser;
-    
+
     public UserController(UserRepository repositoryUser) {
         this.repositoryUser = repositoryUser;
     }
 
     @PostMapping("/criar")
-    public ResponseEntity<UserApi> criarUsuario(@RequestBody UserApi user){
+    public ResponseEntity<UserApi> criarUsuario(@RequestBody UserApi user) {
         UserApi usuarioSalvo = this.repositoryUser.save(user);
         return ResponseEntity.ok(usuarioSalvo);
     }
+
+   
+    
 }
